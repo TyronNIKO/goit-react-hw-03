3913be16cf66ae0d19f5eeed20da233a998296c7
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-08-02 22:12:38 +0300

    Homework added validation

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,16 @@
 import {useId} from "react";
 import css from "./ContactForm.module.css";
+import {Field, Form, Formik, ErrorMessage} from "formik";
+import * as Yup from "yup";
+
+const initialValues = {
+    contactName: "",
+    contactPhone: "",
+};
+const FeedbackSchema = Yup.object().shape({
+    contactName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    contactPhone: Yup.string().min(3, "Too short").max(256, "Too long").required("Required"),
+});
 
 const ContactForm = ({onAdd}) => {
     const idName = useId();
@@ -9,26 +20,30 @@ const ContactForm = ({onAdd}) => {
         e.preventDefault();
         // id: `id-${Date.now()}`,
         onAdd({
-            name: e.target.elements.name.value,
-            number: e.target.elements.phone.value,
+            name: e.target.elements.contactName.value,
+            number: e.target.elements.contactPhone.value,
         });
         e.target.reset();
     };
     return (
         <div className={css["contacts-form"]}>
-            <form onSubmit={handleSubmit} className={css.form}>
-                <div className={css["form-group"]}>
-                    <label htmlFor={idName}>Name</label>
-                    <input type="text" name="name" id={idName} />
-                </div>
-                <div className={css["form-group"]}>
-                    <label htmlFor={idPhone}>Number</label>
-                    <input type="text" name="phone" id={idPhone} />
-                </div>
-                <button type="submit" className={css.btn}>
-                    Add contact
-                </button>
-            </form>
+            <Formik onSubmit={handleSubmit} initialValues={initialValues} validationSchema={FeedbackSchema}>
+                <Form className={css.form}>
+                    <div className={css["form-group"]}>
+                        <label htmlFor={idName}>Name</label>
+                        <Field type="text" name="contactName" id={idName} />
+                        <ErrorMessage name="contactName" component="span" className={css["error-message"]} />
+                    </div>
+                    <div className={css["form-group"]}>
+                        <label htmlFor={idPhone}>Number</label>
+                        <Field type="text" name="contactPhone" id={idPhone} />
+                        <ErrorMessage name="contactPhone" component="span" className={css["error-message"]} />
+                    </div>
+                    <button type="submit" className={css.btn}>
+                        Add contact
+                    </button>
+                </Form>
+            </Formik>
         </div>
     );
 };
